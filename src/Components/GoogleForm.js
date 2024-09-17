63837ebba7f0e67cb970d6bb95c8b1ce08923037
BuildMeAWebsite <contact@buildmeawebsite.ca>
2024-09-17 11:38:43 -0400

    43

--- src/Components/GoogleForm.js
+++ src/Components/GoogleForm.js
@@ -16,7 +16,7 @@ const GoogleFormEmbed = () => {
         width="100%"
       
 
-        height="800px"
+        height="500px"
         frameBorder="0"
         marginHeight="0"
         marginWidth="0"
