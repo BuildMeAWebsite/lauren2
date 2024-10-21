80f7212aac26dbbc62314dc10ca461823eb5553d
BuildMeAWebsite <contact@buildmeawebsite.ca>
2024-10-21 08:22:34 -0400

    3

--- src/Components/Home/HomeSection6.js
+++ src/Components/Home/HomeSection6.js
@@ -14,13 +14,18 @@ const HomeSection6 = () => {
         <div className={styles.contentContainer}>
           <div className={styles.heroSection}>
             <p className={styles.smallText}>hello and <em>welcome</em>.</p>
-            <h1 className={styles.heading}>
-              If you are reading this, you have already taken the biggest step in the therapeutic journey. 
-              My name is Lauren Martyn, I am a registered psychotherapist and I have dedicated myself to supporting school-aged 
-              children, youth, and young adults as they navigate life’s challenges. I currently offer virtual counselling to 
-              those residing in Ontario, Canada.
-            </h1>
+
+            {/* Heading wrapped inside headingWrapper for hover effect */}
+            <div className={styles.headingWrapper}>
+              <h1 className={styles.heading}>
+                If you are reading this, you have already taken the biggest step in the therapeutic journey. 
+                My name is Lauren Martyn, I am a registered psychotherapist and I have dedicated myself to supporting school-aged 
+                children, youth, and young adults as they navigate life’s challenges. I currently offer virtual counselling to 
+                those residing in Ontario, Canada.
+              </h1>
+            </div>
           </div>
+
           <div className={styles.imageContainer}>
             <img
               src="https://res.cloudinary.com/dvbubqhpp/image/upload/v1728154934/lauren1_xmyolw.webp"
