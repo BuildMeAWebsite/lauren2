2208fe43653914f22f2cacc050e94311f038d676
BuildMeAWebsite <contact@buildmeawebsite.ca>
2024-10-22 09:14:18 -0400

    43

--- src/Components/About/AboutSection7.js
+++ src/Components/About/AboutSection7.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Grid, Box, Typography, Button } from '@mui/material';
+import { Grid, Box, Typography } from '@mui/material';
 
 const AboutSection7 = () => {
   return (
     <Box sx={styles.container}>
-      <Grid container spacing={1} alignItems="center">
-        {/* Image on top for smaller screens */}
-        <Grid item xs={12} md={6} sx={{ ...styles.imageContainer, order: { xs: 1, md: 2 } }}>
+      <Grid container spacing={2} alignItems="center">
+        {/* Text on top for smaller screens */}
+        <Grid item xs={12} md={6} sx={{ order: { xs: 1, md: 1 } }}>
+          <Typography variant="body1" sx={styles.smallText}>
+            <em>expertise</em>
+          </Typography>
+          <Typography variant="body1" sx={styles.paragraph}>
+            Currently serving as a School Counsellor at an Independent School, I offer specialized expertise in navigating school environments and addressing the unique challenges they present.
+          </Typography>
+        </Grid>
+        {/* Image on bottom for smaller screens */}
+        <Grid item xs={12} md={6} sx={{ ...styles.imageContainer, order: { xs: 2, md: 2 } }}>
           <img
             src="https://res.cloudinary.com/dvbubqhpp/image/upload/f_auto/v1728154935/lauren4_vo7lr2.webp"
             alt="Lauren Martyn"
@@ -15,16 +24,6 @@ const AboutSection7 = () => {
           <Typography variant="caption" sx={styles.caption}>
             Lauren Martyn RP, MA
           </Typography>
-        </Grid>
-        {/* Text on bottom for smaller screens */}
-        <Grid item xs={12} md={6} sx={{ order: { xs: 1, md: 2 } }}>
-          <Typography variant="body1" sx={styles.smallText}>
-            <em>expertise</em>
-          </Typography>
-          <Typography variant="body1" sx={styles.paragraph}>
-            Currently serving as a School Counsellor at an Independent School, I offer specialized expertise in navigating school environments and addressing the unique challenges they present.
-          </Typography>
-         
         </Grid>
       </Grid>
     </Box>
@@ -41,8 +40,7 @@ const styles = {
   },
   smallText: {
     fontSize: '2.25rem',
-    textShadow: '0px 1px 2px rgba(0, 0, 0, 0.25)',
-
+    textShadow: '0 1px 2px rgba(0, 0, 0, 0.125)',
     letterSpacing: '0.25rem',
     textTransform: 'lowercase',
     marginBottom: '20px',
@@ -59,28 +57,6 @@ const styles = {
     lineHeight: '1.5',
     fontSize: '20px',
     marginBottom: '30px',
-  },
-  button: {
-    fontFamily: "'Lora', sans-serif",
-    boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.25)',
-
-    fontWeight: '300',
-    display: 'inline-block',
-    padding: '10px 30px',
-    border: '1.25px solid #8FBC8F',
-    color: '#2B3D2B',
-    textDecoration: 'none',
-    textTransform: 'uppercase',
-    fontSize: '0.88rem',
-    margin: '0 auto',
-    marginBottom: '20px',
-    textAlign: 'center',
-    transition: 'color 0.3s ease, transform 0.3s ease, border-color 0.3s ease',
-  },
-  buttonHover: {
-    color: '#FFD700', // Yellow text color on hover
-    borderColor: '#FFD700', // Yellow border color on hover
-    transform: 'scale(1.05)', // Slightly enlarge the button on hover
   },
   imageContainer: {
     display: 'flex',
